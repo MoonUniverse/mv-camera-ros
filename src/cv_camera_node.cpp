1c1038882b0853dbe2cb7c77c3b0362d939cf7f2
jinyuying <jinyuyingsspu@gmail.com>
2018-12-13 10:50:42 +0800

    fix birdcamera

--- src/cv_camera_node.cpp
+++ src/cv_camera_node.cpp
@@ -1,19 +1,34 @@
 // Copyright [2015] Takashi Ogura<t.ogura@gmail.com>
 
 #include "driver.h"
+#include <sensor_msgs/TimeReference.h>
+#include "MVCameraCapture.h"
+
+uint32_t triggerCounter;
+ros::Time Imutimestamp;
+void callback(const sensor_msgs::TimeReference::ConstPtr &time_ref) {
+     Imutimestamp = time_ref->header.stamp + ros::Duration(0.05);
+     triggerCounter = time_ref->header.seq;
+}
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "cv_camera");
+
   ros::NodeHandle private_node("~");
+
   cv_camera::Driver driver(private_node, private_node);
+
+  ros::NodeHandle n;
+
+  ros::Subscriber subTimeRef = n.subscribe("/imu/trigger_time", 1000, callback);
 
   try
   {
     driver.setup();
     while (ros::ok())
     {
-      driver.proceed();
+	    driver.proceed();
       ros::spinOnce();
     }
   }
@@ -22,6 +37,7 @@ int main(int argc, char **argv)
     ROS_ERROR_STREAM("cv camera open failed: " << e.what());
     return 1;
   }
+
 
   return 0;
 }
